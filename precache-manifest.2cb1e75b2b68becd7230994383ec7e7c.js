b527c691d9ea6cee63b86a5ba589bf79191e4219
Matthew Knippen <matthew@zwiffer.com>
2020-04-24 14:42:51 -0500

    Updates

---
+++ precache-manifest.2cb1e75b2b68becd7230994383ec7e7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1e2f6f8c7ec2995c922a8ece29c654f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "dee3bb12025e7c51d2ff",
+    "url": "/monsters-rolodex/static/css/main.3c9475c7.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dee3bb12025e7c51d2ff",
+    "url": "/monsters-rolodex/static/js/main.3800790c.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
